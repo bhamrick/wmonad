a0109f3f44123848acf004030706ce2286fc084e
Brian Hamrick <brian.c.hamrick@gmail.com>
2014-10-24 19:04:33 -0700

    Control.WMonad that reexports everything

---
+++ Control/WMonad.hs
@@ -0,0 +1,9 @@
+module Control.WMonad
+    ( module Control.WMonad.Types
+    , module Control.WMonad.Base
+    , module Control.WMonad.Wiring
+    ) where
+
+import Control.WMonad.Base
+import Control.WMonad.Types
+import Control.WMonad.Wiring
